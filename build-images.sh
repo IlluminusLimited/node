5b0ef38f1b7bdf8e2233968dcf1f9359fb76214d
scytherswings <scytherswings@gmail.com>
2018-05-11 11:31:21 -0400

    its all lies but it should work

--- build-images.sh
+++ build-images.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-docker build --file 6/Dockerfile -t pinster/node:latest -t pinster/node:6  -t pinster/awseb:6.10 .
+docker build --file 6.10/Dockerfile -t pinster/awseb:latest -t pinster/awseb:6.10 .
+docker build --file 6/Dockerfile -t pinster/awseb:6 .
