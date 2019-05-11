b72095688eedf5773eb975e90a36fe3791b8077d
scytherswings <scytherswings@gmail.com>
2019-05-10 23:40:45 -0400

    udpated to node 8

--- build-images.sh
+++ build-images.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-docker build --file 6.10/Dockerfile -t pinster/node:latest -t pinster/node:6.10 .
-docker build --file 6/Dockerfile -t pinster/node:6 .
+docker build --file 8.10/Dockerfile -t pinster/node:latest -t pinster/node:8.10 .
